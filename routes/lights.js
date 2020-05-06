ceecb07a6ed8aba6b92a973e1d3ca04e083e12b4
Alexander Rusiecki <alexander.rusiecki@gmail.com>
2020-05-07 00:33:52 +0200

    Added light color changer, added flushHeaders()

--- routes/lights.js
+++ routes/lights.js
@@ -3,6 +3,7 @@ const { db, update } = require("./../db");
 
 const router = new Router();
 
+// Turn light ON or OFF
 router.get("/:id/:state", (req, res) => {
   const id = req.params.id;
   const state = req.params.state === "on" ? true : false;
@@ -11,6 +12,17 @@ router.get("/:id/:state", (req, res) => {
   update();
 
   res.send({ msg: `Light ${id} is now ${req.params.state}` });
+});
+
+// Change light color
+router.get("/:id/color/:value", (req, res) => {
+  const id = req.params.id;
+  const color = `#${req.params.value}`;
+
+  db.get("devices").find({ id: id }).assign({ color: color }).write();
+  update();
+
+  res.send({ msg: `Light ${id} is now HEX color ${color}` });
 });
 
 module.exports = router;
